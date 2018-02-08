a13bd5f83099c348e8bf2ccc9c247ea304962b90
Michael Kovacic <michaelkovacic@Michaels-MacBook-Pro.local>
2018-02-08 15:11:44 -0700

    memory game wins functioning

--- Javascript/gifpy.js
+++ Javascript/gifpy.js
@@ -73,14 +73,19 @@ var cards = [
 
 ];
 
+	// var gifName = [];
+	// var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=4rflNs59030SuauN0DtLlygAYaj36Q6J&q=" + gifName + "&limit=15&offset=0&rating=PG&lang=en";
+
+
+	// $.ajax({
+	// 		url: queryURL,
+	// 		method: "GET"
+	// 	}).then(function(response){
+
 
 //create an initialize function to run at the start of the page, will include reset, shuffle, reset modals,
 // function(initialize){
 
-
-
-
-// }
 
 //functio to put the cards onto the page
 // function(buildHTML){
@@ -89,16 +94,15 @@ $("document").ready(function buildHTML(){
 
 	for (var i = 0; i < 4; i++) {
 	
-	
 		var divContain = $("<div>");
 		divContain.addClass("card flip-container col-md-3");
-		divContain.attr("data-id", cards[i].id);
 		$(".game").append(divContain);
 
 		var insideCard = $("<div>");
 		insideCard.addClass("flipper");
 		insideCard.addClass(cards[i].item);
 		insideCard.attr("id", cards[i].tag);
+		insideCard.attr("data-id", cards[i].id);
 		divContain.append(insideCard);
 
 		var front = $("<div>");
@@ -125,16 +129,15 @@ $("document").ready(function buildHTML(){
 
 	for (var i = 4; i < cards.length; i++) {
 	
-	
 		var divContain = $("<div>");
 		divContain.addClass("card flip-container col-md-3");
-		divContain.attr("data-id", cards[i].id);
 		$(".game2").append(divContain);
 
 		var insideCard = $("<div>");
 		insideCard.addClass("flipper");
 		insideCard.addClass(cards[i].item);
 		insideCard.attr("id", cards[i].tag);
+		insideCard.attr("data-id", cards[i].id);
 		divContain.append(insideCard);
 
 		var front = $("<div>");
@@ -163,77 +166,52 @@ $("document").ready(function buildHTML(){
 
 // var itemNumber = [".one", ".two", ".three", ".four", ".five", ".six", ".seven", ".eight"]
 
+var choice1 = "";
+var choice2 = "";
 
+	$(".flip-container").on("click", ".flipper", function(){
+		console.log(typeof this);
+		$(this).toggleClass("A");
 
+		var choiceId = $(this).attr("data-id");
 
+		if (choice1 == ""){
+			choice1 = choiceId;
+			
+		}else if(choice2 == ""){
+			choice2 = choiceId;
 
+		}
 
+		if (choice1 != "" && choice2 != "") {
 
-	$(".flip-container").on("click", ".one", function(){
-		console.log("works");
-		$(".one").toggleClass("A");
-	});
-	$(".flip-container").on("click", ".two", function(){
-		console.log("works");
-		$(".two").toggleClass("A");
-		
-	});
-	$(".flip-container").on("click", ".three", function(){
-		console.log("works");
-		$(".three").toggleClass("A");
-		
-	});
-	$(".flip-container").on("click", ".four", function(){
-		console.log("works");
-		$(".four").toggleClass("A");
-		
-	});
-	$(".flip-container").on("click", ".five", function(){
-		console.log("works");
-		$(".five").toggleClass("A");
-		
-	});
-	$(".flip-container").on("click", ".six", function(){
-		console.log("works");
-		$(".six").toggleClass("A");
-		
-	});
-	$(".flip-container").on("click", ".seven", function(){
-		console.log("works");
-		$(".seven").toggleClass("A");
-		
-	});
-	$(".flip-container").on("click", ".eight", function(){
-		console.log("works");
-		$(".eight").toggleClass("A");
-		
-	});
+			if(choice1 === choice2){
+				console.log("choice1: " + choice1 + "choice2: " + choice2 + "... win")
+				
+			}else{
+				console.log("choice1: " + choice1 + "choice2: " + choice2 + "... lose")
+				
+				setTimeout(function(){ 
+				$("div.flipper[data-id='" + choice1 + "']").removeClass("A");
+				$("div.flipper[data-id='" + choice2 + "']").removeClass("A");
+				}, 3000);
 
+			}
+			choice1 = "";
+			choice2 = "";
+		}
 
+	});
 	
 
 
-// 	var cardFront = $(this).children(".front");
-// 	var cardBack = $(this).children(".back");
 
-// console.log("works")
-// 	if (cardFront.is(":visible")) {
-// 		console.log("front is visible")
-// 		cardFront.hide();
-// 		cardBack.show();
+function reset (){
+
+	$(".one").removeClass("A");
 
 
-// 	}else{
-// 		console.log("bask is visible")
-// 		console.log(this)
-// 		cardBack.hide();
-// 		cardFront.show();
-// 	}
-
-// 	// $(this).stop();
-
-// })
-
+}
 
 //function to show and hide the cards
 //if the cards match pause or freeze them
