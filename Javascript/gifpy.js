16e299a59fcf989a6a99111f33ce48069f4b2876
Michael Kovacic <michaelkovacic@Michaels-MacBook-Pro.local>
2018-02-08 12:03:59 -0700

    all transitions functioning

--- Javascript/gifpy.js
+++ Javascript/gifpy.js
@@ -5,49 +5,60 @@ var cards = [
 	{
 		name: "flat-triangle",
 		image: "images/if_playstation-flat-icon-triangle-dark_341043.png",
-		id: 1
+		id: 1,
+		tag: "A"
+
+	},
+	
+	{
+		name: "checked-circle",
+		image: "images/if_circle-check_430087.png",
+		id: 2,
+		tag: "C"
+
+	},
+	
+	{
+		name: "triangle-plain",
+		image: "images/if_button_shape_triangle_352894.png",
+		id: 3,
+		tag: "E"
+
+	},
+	
+	{
+		name: "pause-circle",
+		image: "images/if_pause-circle-outline_326570.png",
+		id: 4,
+		tag: "G"
+
+	},
+	{
+		name: "triangle-plain",
+		image: "images/if_button_shape_triangle_352894.png",
+		id: 3,
+		tag: "F"
+
+	},
+	{
+		name: "checked-circle",
+		image: "images/if_circle-check_430087.png",
+		id: 2,
+		tag: "D"
+
+	},
+	{
+		name: "pause-circle",
+		image: "images/if_pause-circle-outline_326570.png",
+		id: 4,
+		tag: "H"
 
 	},
 	{
 		name: "flat-triangle",
-		image: "/images/if_playstation-flat-icon-triangle-dark_341043.png",
-		id: 1
-
-	},
-	{
-		name: "checked-circle",
-		image: "/images/if_circle-check_430087.png",
-		id: 2
-
-	},
-	{
-		name: "checked-circle",
-		image: "/images/if_circle-check_430087.png",
-		id: 2
-
-	},
-	{
-		name: "triangle-plain",
-		image: "/images/if_button_shape_triangle_352894.png",
-		id: 3
-
-	},
-	{
-		name: "triangle-plain",
-		image: "/images/if_button_shape_triangle_352894.png",
-		id: 3
-
-	},
-	{
-		name: "pause-circle",
-		image: "/images/if_pause-circle-outline_326570.png",
-		id: 4
-
-	},
-	{
-		name: "pause-circle",
-		image: "/images/if_pause-circle-outline_326570.png",
-		id: 4
+		image: "images/if_playstation-flat-icon-triangle-dark_341043.png",
+		id: 1,
+		tag: "B"
 
 	}
 
@@ -67,46 +78,85 @@ var cards = [
 	//main card div
 $("document").ready(function buildHTML(){
 
-	for (var i = 0; i < cards.length; i++) {
+	for (var i = 0; i < 4; i++) {
 	
 	
 		var divContain = $("<div>");
-		divContain.addClass("card flip-container");
+		divContain.addClass("card flip-container col-md-3");
 		divContain.attr("data-id", cards[i].id);
 		$(".game").append(divContain);
 
-
-		var insideCard = $("<div>");i
-		insideCard.addClass("inside flipper");
-		$(".card").append(insideCard);
+		var insideCard = $("<div>");
+		insideCard.addClass("flipper");
+		insideCard.attr("id", cards[i].tag);
+		divContain.append(insideCard);
 
 		var front = $("<div>");
 		front.addClass("front");
-		$(".inside").append(front);
-		
+		insideCard.append(front);
+
 		var imageFront = $("<img>");
 		imageFront.attr("src", cards[i].image);
 		imageFront.attr("width", "200px");
 		imageFront.attr("height", "200px");
-		$(".front").append(imageFront);
+		front.append(imageFront);
 
 		var back = $("<div>");
 		back.addClass("back");
 		back.attr("src", "../images/marijuana.png")
-		$(".inside").append(back);
-
+		insideCard.append(back);
+	
 		var imageBack = $("<img>");
 		imageBack.attr("src", "images/marijuana.png");
 		imageBack.attr("width", "200px");
 		imageBack.attr("height", "200px");
-		$(".back").append(imageBack);
+		back.append(imageBack);
+	};
 
-	}
+	for (var i = 4; i < cards.length; i++) {
+	
+	
+		var divContain = $("<div>");
+		divContain.addClass("card flip-container col-md-3");
+		divContain.attr("data-id", cards[i].id);
+		$(".game2").append(divContain);
+
+		var insideCard = $("<div>");
+		insideCard.addClass("flipper");
+		insideCard.attr("id", cards[i].tag);
+		divContain.append(insideCard);
+
+		var front = $("<div>");
+		front.addClass("front");
+		insideCard.append(front);
+
+		var imageFront = $("<img>");
+		imageFront.attr("src", cards[i].image);
+		imageFront.attr("width", "200px");
+		imageFront.attr("height", "200px");
+		front.append(imageFront);
+
+		var back = $("<div>");
+		back.addClass("back");
+		back.attr("src", "../images/marijuana.png")
+		insideCard.append(back);
+	
+		var imageBack = $("<img>");
+		imageBack.attr("src", "images/marijuana.png");
+		imageBack.attr("width", "200px");
+		imageBack.attr("height", "200px");
+		back.append(imageBack);
+	};
 
 });
 
+$(".flipper").on("click", function(){
 
-// };
+	$(this).stop();
+
+})
+
+
 //function to show and hide the cards
 //if the cards match pause or freeze them
 //once all the cards are matched run winner modal
